f3b2d0f0a2cd01cb59451a6ccca5aa624d5b7de5
Malcolm <malcolm9292@gmail.com>
2021-05-03 19:51:23 -0500

    Made backend database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const { User } = require("./models");
 const app = express();
 const port = 8000;
+
+app.get("/Users", async (req, res) => {
+  const users = await User.findAll();
+  res.json(users);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
