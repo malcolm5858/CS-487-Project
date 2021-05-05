821764b7ee2a660f28a00b85e2eaa5c677178a8e
olemeha <77707992+olemeha@users.noreply.github.com>
2021-05-05 00:38:51 -0500

    create profile page

---
+++ customer.js
@@ -0,0 +1,24 @@
+function initMap() {
+	//location of orlando
+	var el = document.getElementById('map');
+	var chicago = { lat: 41.888915316222274, lng:-87.63659613012062};
+	//zoomed in onto the city 
+	var mapSettings = {
+		center: chicago,
+	 	zoom: 11,
+	};
+	var myMap = new google.maps.Map(el, mapSettings);
+
+	/*Create first marker on the Orlando Downtown
+	 *Add event listener with an info window
+	 */
+	var marker = new google.maps.Marker({
+	  position: chicago,
+	  map: myMap,
+	  animation:google.maps.Animation.DROP
+	});
+
+	
+}
+
+google.maps.event.addDomListener(window, 'load', init);
